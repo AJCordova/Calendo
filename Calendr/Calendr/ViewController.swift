d443767cc43f61c14678b3098dd057f85a6177b9
AJCordova <archonist252@gmail.com>
2022-05-10 12:41:22 +0800

    Create calendar project.

---
+++ Calendr/Calendr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calendr
+//
+//  Created by Amiel Jireh Cordova on 5/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemBackground
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
