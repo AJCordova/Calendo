fdbcf0bb558d3d9651b215005e1a06ea3be13b5c
AJCordova <archonist252@gmail.com>
2022-05-12 17:31:21 +0800

    Create constant references for keychain keys and firestore.

---
+++ Calendr/Calendr/Commons/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Calendr
+//
+//  Created by Amiel Jireh Cordova on 5/12/22.
+//
+
+import Foundation
+
+struct Constants {
+    
+    enum FirebaseStrings {
+        static let userCollectionReference = "Users"
+        static let userReference = "email"
+    }
+    
+    enum Keys {
+        static let userInfoKey = "calendr"
+    }
+}
