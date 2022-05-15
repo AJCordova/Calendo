d6fb66794e34ced87fb0ff8a1a3b47e0f8b5447e
AJCordova <archonist252@gmail.com>
2022-05-15 18:39:38 +0800

    Clean up project files.

--- Calendr/Calendr/Models/UserModel.swift
+++ Calendr/Calendr/Models/UserModel.swift
@@ -10,11 +10,4 @@ import Foundation
 struct UserModel: Codable {
     let userName: String
     let password: String
-    let events: [EventModel]
-    
-//    init(userName: String, password: String, events: [EventModel]) {
-//        self.userName = userName
-//        self.password = password
-//        self.events = events
-//    }
 }
