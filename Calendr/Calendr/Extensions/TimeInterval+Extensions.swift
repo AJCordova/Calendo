1fb0650c728b9e9d8f504da0ba826845b26b8700
AJCordova <archonist252@gmail.com>
2022-05-15 18:39:12 +0800

    Remove unused property and methods.

--- Calendr/Calendr/Extensions/TimeInterval+Extensions.swift
+++ Calendr/Calendr/Extensions/TimeInterval+Extensions.swift
@@ -8,19 +8,5 @@
 import Foundation
 
 extension TimeInterval {
-    static var week: TimeInterval {
-        return 7 * 24 * 60 * 60
-    }
-    
-    static func weeks(_ weeks: Double) -> TimeInterval {
-        return weeks * TimeInterval.week
-    }
-    
-    static var month: TimeInterval {
-        return 30 * 24 * 60 * 60
-    }
-    
-    static func months(_ months: Double) -> TimeInterval {
-        return months * TimeInterval.month
-    }
+    static var day: TimeInterval { return 24 * 60 * 60 }
 }
