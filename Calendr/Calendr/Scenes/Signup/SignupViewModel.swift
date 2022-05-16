93c8f11c9050dbb5198851ec74da935b85adfef3
AJCordova <archonist252@gmail.com>
2022-05-16 13:51:13 +0800

    Create and implement user sign up view.

--- Calendr/Calendr/Scenes/Signup/SignupViewModel.swift
+++ Calendr/Calendr/Scenes/Signup/SignupViewModel.swift
@@ -11,13 +11,16 @@ import RxCocoa
 protocol SignupViewModelInputs {
     func emailDidChange(email: String)
     func passwordDidChange(password: String)
-    func confirmPassDidChange(password: String)
+    func confirmPassDidChange(password: String, control: String)
+    func submitRegistration(email: String, hash: String)
+    func goToSignin()
 }
 
 protocol SignupViewModelOutputs {
     var isEmailValid: PublishRelay<TextFieldStatus> { get }
     var isPasswordValid: PublishRelay<TextFieldStatus> { get }
     var doesPasswordMatch: PublishRelay<Bool> { get }
+    var isEmailAvailable: PublishRelay<Bool> { get }
     var invalidEmailMessage: PublishRelay<String> { get }
     var invalidPasswordMessage: PublishRelay<String> { get }
     var mismatchPasswordMessage: PublishRelay<String> { get }
@@ -29,26 +32,38 @@ protocol SignupViewModelTypes {
 }
 
 class SignupViewModel: SignupViewModelInputs, SignupViewModelOutputs, SignupViewModelTypes {
-    
     var inputs: SignupViewModelInputs { return self }
     var outputs: SignupViewModelOutputs { return self }
     
     var isEmailValid: PublishRelay<TextFieldStatus> = PublishRelay()
     var isPasswordValid: PublishRelay<TextFieldStatus> = PublishRelay()
     var doesPasswordMatch: PublishRelay<Bool> = PublishRelay()
+    var isEmailAvailable: PublishRelay<Bool> = PublishRelay()
     var invalidEmailMessage: PublishRelay<String> = PublishRelay()
     var invalidPasswordMessage: PublishRelay<String> = PublishRelay()
     var mismatchPasswordMessage: PublishRelay<String> = PublishRelay()
     
     private var disposeBag = DisposeBag()
+    private var coordinator: SignupCoordinatorDelegate
     private var emailDidChangeProperty = PublishSubject<String>()
     private var passwordDidChangeProperty = PublishSubject<String>()
     private var verifyPassDidChangeProperty = PublishSubject<String>()
+    private var userManager: UserManagementProtocol
     
-    init() {
+    init(coordinate: SignupCoordinatorDelegate) {
+        self.coordinator = coordinate
+        self.userManager = UserManagementService()
+        
         emailDidChangeProperty
             .map { $0.isValidEmail }
             .bind(to: isEmailValid)
+            .disposed(by: disposeBag)
+        
+        emailDidChangeProperty
+            .filter { $0.isValidEmail.isValid }
+            .bind(onNext: { email in
+                self.verifyEmailAvailability(email: email)
+            })
             .disposed(by: disposeBag)
         
         isEmailValid
@@ -63,6 +78,18 @@ class SignupViewModel: SignupViewModelInputs, SignupViewModelOutputs, SignupView
             .bind(to: invalidEmailMessage)
             .disposed(by: disposeBag)
         
+        isEmailAvailable
+            .filter { $0 == true }
+            .map { _ in "" }
+            .bind(to: invalidEmailMessage)
+            .disposed(by: disposeBag)
+        
+        isEmailAvailable
+            .filter { $0 == false }
+            .map { _ in "This email account has been registered. Please use another." }
+            .bind(to: invalidEmailMessage)
+            .disposed(by: disposeBag)
+            
         passwordDidChangeProperty
             .map { $0.count > 7 && $0.count < 21 ? .valid : .invalid }
             .bind(to: isPasswordValid)
@@ -79,6 +106,29 @@ class SignupViewModel: SignupViewModelInputs, SignupViewModelOutputs, SignupView
             .map { _ in "" }
             .bind(to: invalidPasswordMessage)
             .disposed(by: disposeBag)
+        
+        doesPasswordMatch
+            .filter { $0 == false }
+            .map { _ in "This must match with your password above." }
+            .bind(to: mismatchPasswordMessage)
+            .disposed(by: disposeBag)
+        
+        doesPasswordMatch
+            .filter { $0 == true }
+            .map { _ in "" }
+            .bind(to: mismatchPasswordMessage)
+            .disposed(by: disposeBag)
+        
+        userManager.isEmailAvailable
+            .bind(to: isEmailAvailable)
+            .disposed(by: disposeBag)
+        
+        userManager.isSignupSuccessful
+            .filter { $0 == true }
+            .bind(onNext: { _ in
+                self.goToSignin()
+            })
+            .disposed(by: disposeBag)
     }
     
     func emailDidChange(email: String) {
@@ -89,8 +139,26 @@ class SignupViewModel: SignupViewModelInputs, SignupViewModelOutputs, SignupView
         passwordDidChangeProperty.onNext(password)
     }
     
-    func confirmPassDidChange(password: String) {
-        verifyPassDidChangeProperty.onNext(password)
+    func confirmPassDidChange(password: String, control: String) {
+        if password.isEmpty {
+            return
+        } else if password.elementsEqual(control) {
+            doesPasswordMatch.accept(true)
+        } else {
+            doesPasswordMatch.accept(false)
+        }
+    }
+    
+    func submitRegistration(email: String, hash: String) {
+        userManager.userSignup(email: email, hash: hash)
+    }
+    
+    func goToSignin() {
+        coordinator.goToSignin()
+    }
+    
+    private func verifyEmailAvailability(email: String) {
+        userManager.checkIfEmailAvailable(email: email)
     }
 }
 
